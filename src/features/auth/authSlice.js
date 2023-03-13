beb4a1fc25f852b364f451444d2ccf6aa2b00535
Abidur-Rahman-Chowdhury <abid43iiuc@gmail.com>
2023-03-13 13:14:06 +0600

    handled signup and login both email and password and google login

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,5 +1,12 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from 'firebase/auth';
+import auth from '../../firebase/firebase.config';
 const initialState = {
   email: '',
   role: '',
@@ -8,11 +15,96 @@ const initialState = {
   error: '',
 };
 
+export const createUser = createAsyncThunk(
+  'auth/createUser',
+  async ({ email, password }, thunkAPI) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'auth/loginUser',
+  async ({ email, password }, thunkAPI) => {
+    const data = await signInWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+export const googleLogin = createAsyncThunk('auth/googleLogin', async () => {
+  const googleProvider = new GoogleAuthProvider();
+  const data = await signInWithPopup(auth, googleProvider);
+
+  return data.user.email;
+});
+
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
+  name: 'auth',
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.email = '';
+    },
+    setUser: (state, { payload }) => {
+      state.email = payload;
+      state.isLoading = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = '';
+      })
+      .addCase(createUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.email = payload;
+        state.isError = false;
+        state.error = '';
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = true;
+        state.email = '';
+        state.isError = true;
+        state.error = action.error.message;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = '';
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.email = payload;
+        state.isError = false;
+        state.error = '';
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = true;
+        state.email = '';
+        state.isError = true;
+        state.error = action.error.message;
+      })
+      .addCase(googleLogin.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = '';
+      })
+      .addCase(googleLogin.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.email = payload;
+        state.isError = false;
+        state.error = '';
+      })
+      .addCase(googleLogin.rejected, (state, action) => {
+        state.isLoading = true;
+        state.email = '';
+        state.isError = true;
+        state.error = action.error.message;
+      });
+  },
+});
 
-
-})
+export const { logout, setUser } = authSlice.actions;
 
 export default authSlice.reducer;
--- src/layout/main/Navbar.js
+++ src/layout/main/Navbar.js
@@ -1,35 +1,53 @@
-import { signOut } from "firebase/auth";
-import React from "react";
-
-import { Link, useLocation } from "react-router-dom";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { signOut } from 'firebase/auth';
+import { Link, useLocation } from 'react-router-dom';
+import auth from '../../firebase/firebase.config';
+import { logout } from '../../features/auth/authSlice';
 
 const Navbar = () => {
   const { pathname } = useLocation();
-
+  const { email } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const handelSignOut = () => {
+    signOut(auth).then(() => {
+      dispatch(logout());
+    });
+  };
   return (
     <nav
       className={`h-14 fixed w-full z-[999] ${
-        pathname === "/" ? null : "bg-white"
+        pathname === '/' ? null : 'bg-white'
       }`}
     >
-      <ul className='max-w-7xl mx-auto flex gap-3 h-full items-center'>
-        <li className='flex-auto font-semibold text-2xl'>
-          <Link to='/'>Dream Jobs</Link>
+      <ul className="max-w-7xl mx-auto flex gap-3 h-full items-center">
+        <li className="flex-auto font-semibold text-2xl">
+          <Link to="/">Dream Jobs</Link>
         </li>
         <li>
-          <Link className='hover:text-primary' to='/jobs'>
+          <Link className="hover:text-primary" to="/jobs">
             Jobs
           </Link>
         </li>
 
-        <li>
-          <Link
-            className='border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all '
-            to='/login'
+        {email ? (
+          <button
+            onClick={handelSignOut}
+            className="border rounded-xl py-2 px-3 bg-[#691F74] text-white font-bold"
           >
-            Login
-          </Link>
-        </li>
+            {' '}
+            Logout{' '}
+          </button>
+        ) : (
+          <li>
+            <Link
+              className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+              to="/login"
+            >
+              Login
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
